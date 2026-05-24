c7966cc6d0eedc2a7cc6dd18e978a194200ba154
JayYosh <133554802+JayYosh@users.noreply.github.com>
2026-05-24 08:16:03 -0700

    implement point struct

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 glViewport(0, 0, width, height);
 }
 
+struct Point {
+    float x, y;
+};
+
 struct Engine {
     int failedGlfwWindow(GLFWwindow* window) {
         if (window == NULL){
@@ -33,7 +37,11 @@ struct Engine {
             std::cout << "Failed to initialize GLAD" << std::endl;
             return;
         }
+
+        // Register the mouse callback
+        glfwSetMouseButtonCallback(window, mouse_button_callback);
         
+        // for resizing the window
         glViewport(0, 0, 800, 600);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
